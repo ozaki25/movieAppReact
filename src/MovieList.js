c1bf18cee9a1199b96d300a6836a9d47cb1f8313
ozaki <fs65ugumori@yahoo.co.jp>
2019-04-17 08:55:51 +0900

    refactor

--- src/MovieList.js
+++ src/MovieList.js
@@ -6,20 +6,24 @@ function MovieList() {
   const [titleOrder, setTitleOrder] = useState('asc');
   const [yearOrder, setYearOrder] = useState('asc');
 
+  const toggleTitleOrder = () =>
+    setTitleOrder(titleOrder === 'asc' ? 'dsc' : 'asc');
+
+  const toggleYearOrder = () =>
+    setYearOrder(yearOrder === 'asc' ? 'dsc' : 'asc');
+
   const sort = para => {
     if (para === 'name') {
       movies.sort((a, b) =>
         titleOrder === 'asc'
-          ? a.Title.charCodeAt(0) - b.Title.charCodeAt(0)
-          : b.Title.charCodeAt(0) - a.Title.charCodeAt(0),
+          ? b.Title.charCodeAt(0) - a.Title.charCodeAt(0)
+          : a.Title.charCodeAt(0) - b.Title.charCodeAt(0),
       );
-      setTitleOrder(titleOrder === 'asc' ? 'dsc' : 'asc');
     }
     if (para === 'year') {
       movies.sort((a, b) =>
-        yearOrder === 'asc' ? a.Year - b.Year : b.Year - a.Year,
+        yearOrder === 'asc' ? b.Year - a.Year : a.Year - b.Year,
       );
-      setYearOrder(yearOrder === 'asc' ? 'dsc' : 'asc');
     }
     setMovies(movies);
   };
@@ -27,22 +31,24 @@ function MovieList() {
   useEffect(() => {
     fetch('https://www.omdbapi.com/?apikey=a567cf6c&s=love&type=movie')
       .then(res => res.json())
-      .then(res => {
-        setMovies(res.Search);
-      })
+      .then(res => setMovies(res.Search))
       .catch();
   }, []);
+
+  useEffect(() => sort('name'), [titleOrder]);
+
+  useEffect(() => sort('year'), [yearOrder]);
 
   return (
     <div>
       <div className="flex table-header">
         <div>Poster</div>
-        <div onClick={() => sort('name')}>
+        <div onClick={toggleTitleOrder}>
           Title
           <span className="arrow-up" />
           <span className="arrow-down" />
         </div>
-        <div onClick={() => sort('year')}>
+        <div onClick={toggleYearOrder}>
           Year
           <span className="arrow-up" />
           <span className="arrow-down" />
