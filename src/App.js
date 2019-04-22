2ecdd3e036413ff071384164bf3c083bd04e93aa
ozaki <fs65ugumori@yahoo.co.jp>
2019-04-23 08:23:14 +0900

    remove unused file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MovieList from './MovieList';
+import MovieList from './components/MovieList';
 
 class App extends Component {
   render() {
